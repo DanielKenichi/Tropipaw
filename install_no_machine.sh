668c7b2e165b1a1ea63ef8bd883fecbf4ff0fb93
DanielKenichi <danielkttateishi@gmail.com>
2025-05-10 21:22:19 -0300

    feat: adds missing steps for no_machine remote access setup

--- install_no_machine.sh
+++ install_no_machine.sh
@@ -4,4 +4,4 @@ set -a
 source .env
 set +a
 
-ansible-playbook install_no_machine.yaml -i "./inventories/$TARGET.ini"
+ansible-playbook -vvv install_no_machine.yaml -i "./inventories/$TARGET.ini"
--- install_pre_requisites.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -a
-source .env
-set +a
-
-ansible-playbook install_pre_requisites.yaml -i "./inventories/$TARGET.ini"
